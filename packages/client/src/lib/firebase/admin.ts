49c8521d8244782acd6404721bb1fec316a0c99d
keyonnaise <su.smisc14@gmail.com>
2025-03-26 00:43:15 +0900

    🩹 Admin SDK 초기화 방식 변경

--- packages/client/src/lib/firebase/admin.ts
+++ packages/client/src/lib/firebase/admin.ts
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase-admin/firestore";
 const credential = cert({
   projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
   clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 });
 
 const app = getApps().length === 0 ? initializeApp({ credential }) : getApps()[0];
